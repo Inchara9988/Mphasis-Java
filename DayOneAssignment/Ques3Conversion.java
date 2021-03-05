228aee7b6c9e7697039245a95389a9c58d2405c1
inchara9988 <incharagowda9988@gmail.com>
2021-03-05 18:29:07 +0530

    Day one practice assignments

---
+++ DayOneAssignment/Ques3Conversion.java
@@ -0,0 +1,19 @@
+package com.mphasis.com;
+
+public class Ques3Conversion {
+    public static void main(String args[]) {
+        byte b;
+        int i = 257;
+        double d = 323.142;
+
+        b = (byte) i;
+        System.out.println("i and b " + i + " " + b);
+
+        i = (int) d;
+        System.out.println("d and i " + d + " " + i);
+
+        b = (byte) d;
+        System.out.println("d and b " + d + " " + b);
+    }
+
+}
---
+++ DayOneAssignment/Ques3Promotion.java
@@ -0,0 +1,16 @@
+package com.mphasis.com;
+
+public class Ques3Promotion {
+    public static void main(String args[]) {
+        byte d = 42;
+        char f = 'a';
+        short i = 1024;
+        int s = 50000;
+        float c = 5.67f;
+        double b = .1234;
+        double result = (c * d) + (s / f) - (b * i);
+        System.out.println((c * d) + " + " + (s / f) + " - " + (b * i));
+        System.out.println("result = " + result);
+    }
+
+}
